0b2137f30f66bca6201ab8636e8dd4236783d2ec
Vaughn Cedric L. Araneta <vaughncedriclipardoaraneta@gmail.com>
2025-10-07 11:10:09 +0800

    implemented dynamic label styling within Evaluate widget

--- qlogicae_rulex/evaluation.cpp
+++ qlogicae_rulex/evaluation.cpp
@@ -14,8 +14,7 @@ namespace QLogicaeRulex
 	}
 
     void Evaluation::_setup_widgets()
-    {
-
+    {        
         _ui->verticalWidget_2->hide();
         _ui->verticalWidget_2->setVisible(false);
 
@@ -23,22 +22,22 @@ namespace QLogicaeRulex
         series_1_1->append(0, 0);
         series_1_1->append(50, 1);
         series_1_1->append(125, 0);
-        series_1_1->setPen(QPen(QColor(0, 200, 0), 2));
+        series_1_1->setPen(QPen(QColor("#208A3C"), 2));
 
         QLineSeries* series_1_2 = new QLineSeries();
         series_1_2->append(75, 0);
         series_1_2->append(250, 1);
         series_1_2->append(525, 0);
-        series_1_2->setPen(QPen(QColor(0, 0, 200), 2));
+        series_1_2->setPen(QPen(QColor("#3574F0"), 2));
 
         QLineSeries* series_1_3 = new QLineSeries();
         series_1_3->append(475, 0);
         series_1_3->append(2000, 1);
         series_1_3->append(3525, 0);
-        series_1_3->setPen(QPen(QColor(200, 0, 0), 2));
+        series_1_3->setPen(QPen(QColor("#DB3B4B"), 2));
 
-        _vLine_1 = new QLineSeries();       
-        QPen vPen_1(Qt::yellow);
+        _vLine_1 = new QLineSeries();
+        QPen vPen_1(QColor("#FFAF0F"));
         vPen_1.setStyle(Qt::SolidLine);
         vPen_1.setWidth(2);
         _vLine_1->setPen(vPen_1);
@@ -57,9 +56,9 @@ namespace QLogicaeRulex
 
         chart_1->setBackgroundVisible(false);
         chart_1->setPlotAreaBackgroundVisible(false);
-        chart_1->setBackgroundPen(QPen(Qt::lightGray));
+        chart_1->setBackgroundPen(QPen(QColor("#9DA0A8")));
 
-        QBrush textBrush_1(Qt::lightGray);
+        QBrush textBrush_1(QColor("#9DA0A8"));
         chart_1->setTitleBrush(textBrush_1);
         chart_1->axes(Qt::Horizontal).first()->setLabelsBrush(textBrush_1);
         chart_1->axes(Qt::Vertical).first()->setLabelsBrush(textBrush_1);
@@ -76,22 +75,22 @@ namespace QLogicaeRulex
         series_2_1->append(0, 0);
         series_2_1->append(20, 1);
         series_2_1->append(50, 0);
-        series_2_1->setPen(QPen(QColor(0, 200, 0), 2));
+        series_2_1->setPen(QPen(QColor("#208A3C"), 2));
 
         QLineSeries* series_2_2 = new QLineSeries();
         series_2_2->append(30, 0);
         series_2_2->append(60, 1);
         series_2_2->append(90, 0);
-        series_2_2->setPen(QPen(QColor(0, 0, 200), 2));
+        series_2_2->setPen(QPen(QColor("#3574F0"), 2));
 
         QLineSeries* series_2_3 = new QLineSeries();
         series_2_3->append(70, 0);
         series_2_3->append(500, 1);
         series_2_3->append(1010, 0);
-        series_2_3->setPen(QPen(QColor(200, 0, 0), 2));
+        series_2_3->setPen(QPen(QColor("#DB3B4B"), 2));
 
         _vLine_2 = new QLineSeries();
-        QPen vPen_2(Qt::yellow);
+        QPen vPen_2(QColor("#FFAF0F"));
         vPen_2.setStyle(Qt::SolidLine);
         vPen_2.setWidth(2);
         _vLine_2->setPen(vPen_2);
@@ -110,9 +109,9 @@ namespace QLogicaeRulex
 
         chart_2->setBackgroundVisible(false);
         chart_2->setPlotAreaBackgroundVisible(false);
-        chart_2->setBackgroundPen(QPen(Qt::lightGray));
+        chart_2->setBackgroundPen(QPen(QColor("#9DA0A8")));
 
-        QBrush textBrush_2(Qt::lightGray);
+        QBrush textBrush_2(QColor("#9DA0A8"));
         chart_2->setTitleBrush(textBrush_2);
         chart_2->axes(Qt::Horizontal).first()->setLabelsBrush(textBrush_2);
         chart_2->axes(Qt::Vertical).first()->setLabelsBrush(textBrush_2);
@@ -129,22 +128,22 @@ namespace QLogicaeRulex
         series_3_1->append(0, 0);
         series_3_1->append(10, 1);
         series_3_1->append(25, 0);
-        series_3_1->setPen(QPen(QColor(0, 200, 0), 2));
+        series_3_1->setPen(QPen(QColor("#208A3C"), 2));
 
         QLineSeries* series_3_2 = new QLineSeries();
         series_3_2->append(15, 0);
         series_3_2->append(30, 1);
         series_3_2->append(45, 0);
-        series_3_2->setPen(QPen(QColor(0, 0, 200), 2));
+        series_3_2->setPen(QPen(QColor("#3574F0"), 2));
 
         QLineSeries* series_3_3 = new QLineSeries();
         series_3_3->append(35, 0);
         series_3_3->append(60, 1);
         series_3_3->append(85, 0);
-        series_3_3->setPen(QPen(QColor(200, 0, 0), 2));
+        series_3_3->setPen(QPen(QColor("#DB3B4B"), 2));
 
         _vLine_3 = new QLineSeries();
-        QPen vPen_3(Qt::yellow);
+        QPen vPen_3(QColor("#FFAF0F"));
         vPen_3.setStyle(Qt::SolidLine);
         vPen_3.setWidth(2);
         _vLine_3->setPen(vPen_3);
@@ -163,9 +162,9 @@ namespace QLogicaeRulex
 
         chart_3->setBackgroundVisible(false);
         chart_3->setPlotAreaBackgroundVisible(false);
-        chart_3->setBackgroundPen(QPen(Qt::lightGray));
+        chart_3->setBackgroundPen(QPen(QColor("#9DA0A8")));
 
-        QBrush textBrush_3(Qt::lightGray);
+        QBrush textBrush_3(QColor("#9DA0A8"));
         chart_3->setTitleBrush(textBrush_3);
         chart_3->axes(Qt::Horizontal).first()->setLabelsBrush(textBrush_3);
         chart_3->axes(Qt::Vertical).first()->setLabelsBrush(textBrush_3);
@@ -185,9 +184,41 @@ namespace QLogicaeRulex
     }
 
     void Evaluation::_on_input_changed()
-    {
+    {        
         _ui->verticalWidget_2->hide();
         _ui->verticalWidget_2->setVisible(false);
+
+        std::string style_1 =
+            "QLabel{"
+            "color: #EBECF0;"
+            "background: #208A3C;"
+            "padding: 2px 8px;"
+            "border-radius: 4px;"
+            "}";
+
+        std::string style_2 =
+            "QLabel{"
+            "color: #EBECF0;"
+            "background: #3574F0;"
+            "padding: 2px 8px;"
+            "border-radius: 4px;"
+            "}";
+
+        std::string style_3 =
+            "QLabel{"
+            "color: #EBECF0;"
+            "background: #DB3B4B;"
+            "padding: 2px 8px;"
+            "border-radius: 4px;"
+            "}";
+
+        std::string style_4 =
+            "QLabel{"
+            "color: #1E1F22;"
+            "background: #FFAF0F;"
+            "padding: 2px 8px;"
+            "border-radius: 4px;"
+            "}";
 
         QString file_name = QFileDialog::getOpenFileName(
             this,
@@ -223,7 +254,31 @@ namespace QLogicaeRulex
         }
         _ui->label_31->setText(QString::fromStdString(label_text));
         _ui->label_30->setText(QString::fromStdString(absl::StrCat(output.code_lexer_output.line_count)));
-        
+        if (size_a > 1)
+        {
+            _ui->label_31->setStyleSheet(QString::fromStdString(style_4));
+            _ui->label_30->setStyleSheet(QString::fromStdString(style_4));
+        }
+        else if (size_a == 1)
+        {
+            std::string variable = output.selected_line_count_level_input_variables[0];
+            if (variable == "small")
+            {
+                _ui->label_31->setStyleSheet(QString::fromStdString(style_1));
+                _ui->label_30->setStyleSheet(QString::fromStdString(style_1));
+            }
+            else if (variable == "medium")
+            {
+                _ui->label_31->setStyleSheet(QString::fromStdString(style_2));
+                _ui->label_30->setStyleSheet(QString::fromStdString(style_2));
+            }
+            else if (variable == "large")
+            {
+                _ui->label_31->setStyleSheet(QString::fromStdString(style_3));
+                _ui->label_30->setStyleSheet(QString::fromStdString(style_3));
+            }
+        }
+
         _vLine_1->clear();
         _vLine_1->append(output.code_lexer_output.line_count, 0);
         _vLine_1->append(output.code_lexer_output.line_count, 1);
@@ -249,9 +304,48 @@ namespace QLogicaeRulex
         }
         _ui->label_34->setText(QString::fromStdString(label_text));
         _ui->label_33->setText(QString::fromStdString(absl::StrCat(output.code_lexer_output.longest_line_size)));
+        if (size_a > 1)
+        {
+            _ui->label_34->setStyleSheet(QString::fromStdString(style_4));
+            _ui->label_33->setStyleSheet(QString::fromStdString(style_4));
+        }
+        else if (size_a == 1)
+        {
+            std::string variable = output.selected_longest_line_size_level_input_variables[0];
+            if (variable == "short")
+            {
+                _ui->label_34->setStyleSheet(QString::fromStdString(style_1));
+                _ui->label_33->setStyleSheet(QString::fromStdString(style_1));
+            }
+            else if (variable == "medium")
+            {
+                _ui->label_34->setStyleSheet(QString::fromStdString(style_2));
+                _ui->label_33->setStyleSheet(QString::fromStdString(style_2));
+            }
+            else if (variable == "long")
+            {
+                _ui->label_34->setStyleSheet(QString::fromStdString(style_3));
+                _ui->label_33->setStyleSheet(QString::fromStdString(style_3));
+            }
+        }
 
         _ui->label_35->setText(QString::fromStdString(output.final_organized_level_outptut_variable));
         _ui->label_16->setText(QString::fromStdString(absl::StrCat(output.centroid)));
+        if (output.final_organized_level_outptut_variable == "organized")
+        {
+            _ui->label_35->setStyleSheet(QString::fromStdString(style_1));
+            _ui->label_16->setStyleSheet(QString::fromStdString(style_1));
+        }
+        else if (output.final_organized_level_outptut_variable == "mixed")
+        {
+            _ui->label_35->setStyleSheet(QString::fromStdString(style_2));
+            _ui->label_16->setStyleSheet(QString::fromStdString(style_2));
+        }
+        else if (output.final_organized_level_outptut_variable == "unorganized")
+        {
+            _ui->label_35->setStyleSheet(QString::fromStdString(style_3));
+            _ui->label_16->setStyleSheet(QString::fromStdString(style_3));
+        }
 
         size_a = output.selected_line_count_level_input_variables.size(),
             size_b = size_a - 1;
@@ -265,6 +359,26 @@ namespace QLogicaeRulex
             }
         }
         _ui->label_19->setText(QString::fromStdString(label_text));
+        if (size_a > 1)
+        {
+            _ui->label_19->setStyleSheet(QString::fromStdString(style_4));
+        }
+        else if (size_a == 1)
+        {
+            std::string variable = output.selected_line_count_level_input_variables[0];
+            if (variable == "small")
+            {
+                _ui->label_19->setStyleSheet(QString::fromStdString(style_1));
+            }
+            else if (variable == "medium")
+            {
+                _ui->label_19->setStyleSheet(QString::fromStdString(style_2));
+            }
+            else if (variable == "large")
+            {
+                _ui->label_19->setStyleSheet(QString::fromStdString(style_3));
+            }
+        }
 
         size_a = output.selected_longest_line_size_level_input_variables.size(),
             size_b = size_a - 1;
@@ -278,6 +392,26 @@ namespace QLogicaeRulex
             }
         }
         _ui->label_20->setText(QString::fromStdString(label_text));
+        if (size_a > 1)
+        {
+            _ui->label_20->setStyleSheet(QString::fromStdString(style_4));
+        }
+        else if (size_a == 1)
+        {
+            std::string variable = output.selected_longest_line_size_level_input_variables[0];
+            if (variable == "short")
+            {
+                _ui->label_20->setStyleSheet(QString::fromStdString(style_1));
+            }
+            else if (variable == "medium")
+            {
+                _ui->label_20->setStyleSheet(QString::fromStdString(style_2));
+            }
+            else if (variable == "long")
+            {
+                _ui->label_20->setStyleSheet(QString::fromStdString(style_3));
+            }
+        }
 
         index_a = 0;
         size_a = output.defuzzified_organized_level_outptut_results.size(),
@@ -292,10 +426,9 @@ namespace QLogicaeRulex
             }
             ++index_a;
         }        
-        _ui->label_29->setText(QString::fromStdString(label_text));
-        
+        _ui->label_29->setText(QString::fromStdString(label_text));        
         _ui->label_28->setText(QString::fromStdString(absl::StrCat(output.centroid)));
-
+        
         index_a = 0;
         size_a = output.degree_of_truths.size(),
             size_b = size_a - 1;
@@ -310,11 +443,58 @@ namespace QLogicaeRulex
             ++index_a;
         }
         _ui->label_27->setText(QString::fromStdString(label_text));
+        if (size_a > 1)
+        {
+            _ui->label_29->setStyleSheet(QString::fromStdString(style_4));
+            _ui->label_28->setStyleSheet(QString::fromStdString(style_4));
+            _ui->label_27->setStyleSheet(QString::fromStdString(style_4));
+        }
+        else if (size_a == 1)
+        {
+            for (const auto& [key, value] : output.defuzzified_organized_level_outptut_results)
+            {
+                if (key == "organized")
+                {
+                    _ui->label_29->setStyleSheet(QString::fromStdString(style_1));
+                    _ui->label_28->setStyleSheet(QString::fromStdString(style_1));
+                    _ui->label_27->setStyleSheet(QString::fromStdString(style_1));
+                    break;
+                }
+                else if (key == "mixed")
+                {
+                    _ui->label_29->setStyleSheet(QString::fromStdString(style_2));
+                    _ui->label_28->setStyleSheet(QString::fromStdString(style_2));
+                    _ui->label_27->setStyleSheet(QString::fromStdString(style_2));
+                    break;
+                }
+                else if (key == "unorganized")
+                {
+                    _ui->label_29->setStyleSheet(QString::fromStdString(style_3));
+                    _ui->label_28->setStyleSheet(QString::fromStdString(style_3));
+                    _ui->label_27->setStyleSheet(QString::fromStdString(style_3));
+                    break;
+                }
+            }
+        }
 
         _ui->label_25->setText(QString::fromStdString(output.final_organized_level_outptut_variable));
         _ui->label_26->setText(QString::fromStdString(absl::StrCat(output.final_organized_level_outptut_value)));
 
-        
+        if (output.final_organized_level_outptut_variable == "organized")
+        {
+            _ui->label_25->setStyleSheet(QString::fromStdString(style_1));
+            _ui->label_26->setStyleSheet(QString::fromStdString(style_1));
+        }
+        else if (output.final_organized_level_outptut_variable == "mixed")
+        {
+            _ui->label_25->setStyleSheet(QString::fromStdString(style_2));
+            _ui->label_26->setStyleSheet(QString::fromStdString(style_2));
+        }
+        else if (output.final_organized_level_outptut_variable == "unorganized")
+        {
+            _ui->label_25->setStyleSheet(QString::fromStdString(style_3));
+            _ui->label_26->setStyleSheet(QString::fromStdString(style_3));
+        }
 
         _ui->verticalWidget_2->show();
         _ui->verticalWidget_2->setVisible(true);
